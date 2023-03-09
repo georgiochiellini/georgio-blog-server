e3ba214f5343332989b6c17c7819c2413bc77abf
havriluke <havrylyuk.post@gmail.com>
2023-03-09 23:00:36 +0200

    mobile

--- middlewares/requestMiddleware.js
+++ middlewares/requestMiddleware.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next) {
         next()
     }
     const isAllowedOrigin = req.header('Origin') === url.URL && !req.useragent.isBot
-    const isAllowedDevice = req.useragent.isDesktop
+    const isAllowedDevice = req.useragent.isDesktop && !req.useragent.isMobile && req.useragent.os.toLowerCase().split(' ')[0] === 'windows'
     
     if (!isAllowedOrigin) return res.status(403).json({message: 'Forbidden'})
     else if (!isAllowedDevice) return res.status(400).json({message: 'Forbidden for mobile users'})
