c626c98077cae4a7463d2c802f4689bfc9654dbc
havriluke <havrylyuk.post@gmail.com>
2023-03-09 22:37:32 +0200

    url

--- utils/url.js
+++ utils/url.js
@@ -3,7 +3,7 @@ const type = 'production'
 
 const url = {
     developing: 'http://localhost:3000',
-    production: 'https://watchmachine-promoting.onrender.com'
+    production: 'https://watch-machine.onrender.com'
 }
 
 module.exports = {URL: url[type]}
